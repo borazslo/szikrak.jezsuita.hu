820e654bebfec91daa0d74d92a53dd408f52b1c4
borazslo <eleklaszlosj@gmail.com>
2021-09-05 19:26:27 +0200

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
-<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
 <?php
 	include 'sql.php'; 
